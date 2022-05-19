cd2f339617a9ec861f217dcb2d60839eeca89196
Dvs <satyasri.125110044@gmail.com>
2022-05-19 16:52:26 +0530

    added baloon messages

--- src/app/pages/chat-box/chat-box.component.ts
+++ src/app/pages/chat-box/chat-box.component.ts
@@ -16,11 +16,71 @@ export class ChatBoxComponent implements OnInit {
 
   constructor() {
     this.contactsMenuItems = [
-      { label: 'New', icon: 'pi pi-fw pi-plus' },
-      { label: 'Open', icon: 'pi pi-fw pi-download' },
-      { label: 'Undo', icon: 'pi pi-fw pi-refresh' },
+      { label: 'New Contact', icon: 'pi pi-fw pi-user-plus' },
+      { label: 'Edit Contact', icon: 'pi pi-fw pi-user-edit' },
+      { label: 'Delete Contact', icon: 'pi pi-fw pi-trash' },
     ];
     this.contacts = [
+      {
+        name: 'Dvs',
+        id: Math.random().toString(),
+        msg: { message: 'Hello', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Davuloori Sai Durga Hemanth',
+        id: Math.random().toString(),
+        msg: { message: 'Hello good morning how are doing? is everything okay?', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Dvs',
+        id: Math.random().toString(),
+        msg: { message: 'Hello', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Davuloori Sai Durga Hemanth',
+        id: Math.random().toString(),
+        msg: { message: 'Hello good morning how are doing? is everything okay?', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Dvs',
+        id: Math.random().toString(),
+        msg: { message: 'Hello', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Davuloori Sai Durga Hemanth',
+        id: Math.random().toString(),
+        msg: { message: 'Hello good morning how are doing? is everything okay?', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Dvs',
+        id: Math.random().toString(),
+        msg: { message: 'Hello', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Davuloori Sai Durga Hemanth',
+        id: Math.random().toString(),
+        msg: { message: 'Hello good morning how are doing? is everything okay?', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Dvs',
+        id: Math.random().toString(),
+        msg: { message: 'Hello', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Davuloori Sai Durga Hemanth',
+        id: Math.random().toString(),
+        msg: { message: 'Hello good morning how are doing? is everything okay?', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Dvs',
+        id: Math.random().toString(),
+        msg: { message: 'Hello', messageDate: new Date(), read: false },
+      },
+      {
+        name: 'Davuloori Sai Durga Hemanth',
+        id: Math.random().toString(),
+        msg: { message: 'Hello good morning how are doing? is everything okay?', messageDate: new Date(), read: false },
+      },
       {
         name: 'Dvs',
         id: Math.random().toString(),
